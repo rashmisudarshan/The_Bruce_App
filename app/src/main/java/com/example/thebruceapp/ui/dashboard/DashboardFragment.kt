663640e2b76ac241e64c533bd9f35fd4fb3f5aaf
Rashmi Sudarshan <rashmisudarshan@gmail.com>
2021-08-09 17:07:03 +0530

    Fixed the Quotes page for minor cosmetic changes

--- app/src/main/java/com/example/thebruceapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/thebruceapp/ui/dashboard/DashboardFragment.kt
@@ -1,45 +1,82 @@
 package com.example.thebruceapp.ui.dashboard
 
+import com.example.thebruceapp.R
+import com.example.thebruceapp.databinding.FragmentDashboardBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.thebruceapp.R
-import com.example.thebruceapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var currentImage:Int = 6
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        val rollButton: ImageButton = binding.button2
+        rollButton.setOnClickListener {
+            rollDice(currentImage)
+        }
+
+
+        rollDice(currentImage)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    private fun rollDice(current: Int) {
+        val dice = Dice(6)
+        val diceRoll = dice.roll(current)
+        if (currentImage!=6)
+            currentImage++
+        else
+            currentImage=1
+        val resultImageView: ImageView = binding.imageView
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.card1
+            2 -> R.drawable.card2
+            3 -> R.drawable.card3
+            4 -> R.drawable.card4
+            5 -> R.drawable.card5
+            else -> R.drawable.card6
+        }
+        resultImageView.setImageResource(drawableResource)
+        resultImageView.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(curr: Int): Int {
+        return when (curr) {
+            1 -> 2
+            2 -> 3
+            3 -> 4
+            4 -> 5
+            5 -> 6
+            else -> 1
+        }
     }
 }
